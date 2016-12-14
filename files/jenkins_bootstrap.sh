d98c0a82b3ed7241b2126093ca9697c287048d42
James Turnbull <james@lovedthanlost.net>
2016-12-14 14:49:59 -0500

    helps if we say yes

--- files/jenkins_bootstrap.sh
+++ files/jenkins_bootstrap.sh
@@ -2,6 +2,6 @@
 
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get -y update
+sudo apt-get -y install jenkins
 
