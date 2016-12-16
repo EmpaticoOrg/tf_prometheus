37762567c00d85a5ee16699cddf5d2d5de17ee8b
James Turnbull <james@lovedthanlost.net>
2016-12-16 18:10:02 -0500

    Fixed boot

--- files/prometheus_bootstrap.sh
+++ files/prometheus_bootstrap.sh
@@ -9,7 +9,7 @@ class { '::prometheus::node_exporter':
 
 class { '::prometheus':
   global_config  => { 'scrape_interval'=> '5s', 'evaluation_interval'=> '15s', 'external_labels'=> { 'monitor'=>'primary'}},
-  scrape_configs => [ { 'job_name'=> 'prometheus', 'scrape_interval'=> '1s', 'scrape_timeout'=> '1s', 'target_groups'=> [ { 'targets'=> [ 'localhost:9090' ], 'labels'=> { 'alias'=> 'Prometheus'} } ] }, { 'job_name'=> 'host', 'ec2_sd_configs'=> { 'region'=> 'us-east=1', 'port'=> 9100 } } ]
+  scrape_configs => [ { 'job_name'=> 'prometheus', 'scrape_interval'=> '1s', 'scrape_timeout'=> '1s', 'static_configs'=> [ { 'targets'=> [ 'localhost:9090' ], 'labels'=> { 'alias'=> 'Prometheus'} } ] }, { 'job_name'=> 'host', 'ec2_sd_configs'=> [ { 'region'=> 'us-east-1', 'port'=> 9100 } ] } ]
 }
 EOF
 
