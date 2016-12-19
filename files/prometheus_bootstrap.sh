4540e07636c6bd2c4b22ea8fd841b9e586365ecf
James Turnbull <james@lovedthanlost.net>
2016-12-18 22:24:08 -0500

    Removed grafana

--- files/prometheus_bootstrap.sh
+++ files/prometheus_bootstrap.sh
@@ -9,10 +9,6 @@ class { '::prometheus':
   global_config  => { 'scrape_interval' => '5s', 'evaluation_interval' => '15s', 'external_labels' => { 'monitor' => 'primary'}},
   scrape_configs => [ { 'job_name' => 'prometheus', 'scrape_interval' => '1s', 'scrape_timeout' => '1s', 'static_configs' => [ { 'targets' => [ 'localhost:9090' ], 'labels' => { 'alias' => 'Prometheus'} } ] }, { 'job_name' => 'host', 'ec2_sd_configs' => [ { 'region' => 'us-east-1', 'port' => 9100 } ] } ]
 }
-
-class { '::prometheus::collectd_exporter': }
-
-class { 'grafana': version => '4.0.2-1481203731', }
 EOF
 
 echo "Running Puppet..."
